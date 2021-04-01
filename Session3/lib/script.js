4d0f5c34a15d38d75bfb50b224ad154275a00044
Michelle Slee <sleemichelle@gmail.com>
2021-04-01 12:07:03 +0100

    Finished session 5 code

--- Session3/lib/script.js
+++ Session3/lib/script.js
@@ -1,54 +1,33 @@
 
-     var canvas = document.getElementById("canvas");
-     var context = canvas.getContext("2d");
+var canvas1 = document.getElementById("canvas1");
+var context1 = canvas1.getContext("2d");
 
-     context.strokeStyle = "green";
-     context.font = '50px serif';
-     context.strokeText("Ghost Chase", 150, 200);
+var canvas2 = document.getElementById("canvas2");
+var context2 = canvas2.getContext("2d");
 
-     context.fillStyle = "purple";
-     context.font = 'bold 25px Arial';
-     context.fillText("Click screen to start", 160, 300);
+canvas2.classList.add('hide');
 
-      canvas.onclick = function() {
-        alert("Game starting...");
-     }
+//Start screen using Canvas1
+context1.strokeStyle = "green";
+context1.font = '50px serif';
+context1.strokeText("Ghost Chase", 150, 200);
 
+context1.fillStyle = "purple";
+context1.font = 'bold 25px Arial';
+context1.fillText("Click screen to start", 160, 300);
 
+//Hide first screen on Click & show Canvas2
+canvas1.onclick = function() {
+  canvas1.classList.add('hide');
+  canvas2.classList.add('show');
+}
 
+// Loading player image
 
-// var canvas1 = document.getElementById("canvas1");
-// var canvas2 = document.getElementById("canvas2");
-// var context1 = canvas1.getContext("2d");
-// var context2 = canvas2.getContext("2d");
-//
-// canvas2.classList.add('hide');
-//
-// // //Start screen using Canvas1
-// context1.strokeStyle = "green";
-// context1.font = '50px serif';
-// context1.strokeText("Ghost Chase", 150, 200);
-//
-// context1.fillStyle = "purple";
-// context1.font = 'bold 25px Arial';
-// context1.fillText("Click screen to start", 160, 300);
-//
-// // Hide first screen on Click & show Canvas2
-// canvas1.onclick = function() {
-//   canvas1.classList.add('hide');
-//   canvas2.classList.add('show');
-// }
-//
-// //Placeholder text for Canvas2
-// context2.strokeStyle = "red";
-// context2.font = '50px serif';
-// context2.strokeText("Game Screen!!!", 150, 200);
-//
-//
-// // Loading player image
-// var playerImage = new Image();
-// playerImage.src = "https://i.postimg.cc/hGm38dT4/spritenormalright.png";
-//
-// playerImage.onload = function() {
-//   context2.drawImage(playerImage, 0, 20, 64,64);
-// };
+var playerImage = new Image();
+
+playerImage.src = "https://i.postimg.cc/hGm38dT4/spritenormalright.png";
+
+playerImage.onload = function() {
+  context2.drawImage(playerImage, 100, 100, 64,64);
+ };
--- Session5/lib/script.js
+++ Session5/lib/script.js
@@ -1,11 +1,13 @@
+
 var canvas1 = document.getElementById("canvas1");
-var canvas2 = document.getElementById("canvas2");
 var context1 = canvas1.getContext("2d");
+
+var canvas2 = document.getElementById("canvas2");
 var context2 = canvas2.getContext("2d");
 
 canvas2.classList.add('hide');
 
-//Start screen using Canvas1 & Context1
+//Start screen using Canvas1
 context1.strokeStyle = "green";
 context1.font = '50px serif';
 context1.strokeText("Ghost Chase", 150, 200);
@@ -14,59 +16,84 @@ context1.fillStyle = "purple";
 context1.font = 'bold 25px Arial';
 context1.fillText("Click screen to start", 160, 300);
 
-
-//Hide first screen on Click & show Canvas2/Context2
+//Hide first screen on Click & show Canvas2
 canvas1.onclick = function() {
   canvas1.classList.add('hide');
-  console.log("Canvas1 gone");
   canvas2.classList.add('show');
-  console.log("Now using canvas2");
-  }
-//==============================================================//
+}
+
+// Loading player image
+var playerImage = new Image();
+playerImage.src = "https://i.postimg.cc/hGm38dT4/spritenormalright.png";
+
 //Variables
 var playerScore = 0;
 var enemyScore = 0;
 
-//Temp code to demo updating playerScore
-canvas2.onclick = function() {
-  playerScore++;
-  console.log(playerScore);
-}
+//Check player image ready & call render()
+playerImage.onload = render();
 
-//Loading player image
-var playerImage = new Image();
-playerImage.src = "https://i.postimg.cc/hGm38dT4/spritenormalright.png";
+//This function draws the background, the scoreboard & the player Sprite & then is called again & again by requestAnimationFrame()
+ function render(){
 
-
-playerImage.onload = checkReady();
-
- //Check Ready & PlayGame
- function checkReady(){
-    playerImage.ready=true;
-    playGame();
-  }
-
-  function playGame(){
-    render();
-    requestAnimationFrame(playGame);
-  }
-
-
-
-  //=============================================================//
-
-  function render(){
-
-    //Black background
+   //Black background
     context2.fillStyle = "black";
     context2.fillRect(0,0, 600, 400);
 
-    //Scoreboard
+   //Scoreboard
     context2.font = "20px Arial";
-    context2.fillStyle = "green";
+    context2.fillStyle = "white";
     context2.fillText("Player: " + playerScore + " Enemy: " + enemyScore, 10, 20);
 
    //Player Sprite
-    context2.drawImage(playerImage, 0, 20, 64,64);
+    //context2.drawImage(playerImage, 100, 100, 64,64);
+    context2.drawImage(playerImage, playerX, playerY, 64,64);
 
-}
+
+//Redraw screen
+    requestAnimationFrame(render);
+  }
+
+  //Update playerScore when screen clicked
+   canvas2.onclick = function() {
+     playerScore++;
+    }
+
+
+
+    //Extra Variables - update drawImage with these
+
+    var playerX = 100;
+    var playerY = 100;
+
+
+      //Key Down event demo
+      document.addEventListener('keydown', function(event) {
+        console.log(event);
+      });
+
+
+      document.addEventListener('keydown', function(event) {
+        if (event.key == "w"){
+          console.log("W was pressed");
+        } else {
+          console.log("W was not pressed");
+        }
+       });
+
+
+    //Player Move
+    document.addEventListener('keydown', function (event) {
+      if(event.key === "w"){
+         playerY-=10;
+      }
+      if(event.key === "s"){
+        playerY+=10;
+     }
+     if(event.key === "a"){
+       playerX-=10;
+    }
+    if(event.key === "d"){
+      playerX+=10;
+      }
+    });
