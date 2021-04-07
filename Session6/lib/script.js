c99d22a7006e63f76eda49ff173722c3b5b12329
Michelle Slee <sleemichelle@gmail.com>
2021-04-07 11:32:56 +0100

    Finished session 6

--- Session6/lib/script.js
+++ Session6/lib/script.js
@@ -1,11 +1,13 @@
+
 var canvas1 = document.getElementById("canvas1");
-var canvas2 = document.getElementById("canvas2");
 var context1 = canvas1.getContext("2d");
+
+var canvas2 = document.getElementById("canvas2");
 var context2 = canvas2.getContext("2d");
 
 canvas2.classList.add('hide');
 
-//Start screen using Canvas1 & Context1
+//Start screen using Canvas1
 context1.strokeStyle = "green";
 context1.font = '50px serif';
 context1.strokeText("Ghost Chase", 150, 200);
@@ -14,92 +16,88 @@ context1.fillStyle = "purple";
 context1.font = 'bold 25px Arial';
 context1.fillText("Click screen to start", 160, 300);
 
-
-//Hide first screen on Click & show Canvas2/Context2
+//Hide first screen on Click & show Canvas2
 canvas1.onclick = function() {
   canvas1.classList.add('hide');
-  console.log("Canvas1 gone");
   canvas2.classList.add('show');
-  console.log("Now using canvas2");
-  }
-//==============================================================//
-//Variables
-var playerScore = 0;
-var enemyScore = 0;
-var playerX = 0;
-var playerY = 20;
+}
 
-//Loading player image
+// Loading player image
 var playerImage = new Image();
 playerImage.src = "https://i.postimg.cc/hGm38dT4/spritenormalright.png";
 
+var crystalImage = new Image();
+crystalImage.src = "https://i.postimg.cc/nc52ggMs/diamond-417896-640.png";
 
-playerImage.onload = checkReady();
+//Variables
+var playerScore = 0;
+var enemyScore = 0;
+var playerX = 100;
+var playerY = 100;
+var crystalX = Math.floor(Math.random() * 450);
+var crystalY = Math.floor(Math.random() * 250);
 
- //Check Ready & PlayGame
- function checkReady(){
-    playerImage.ready=true;
-    playGame();
-  }
+//Check player image ready & call render()
+playerImage.onload = render();
 
-  function playGame(){
-    render();
-    requestAnimationFrame(playGame);
-  }
+//This function draws the background, the scoreboard & the player Sprite & then is called again & again by requestAnimationFrame()
+ function render(){
 
-  //=============================================================//
-
-  function render(){
-
-    //Black background
+   //Black background
     context2.fillStyle = "black";
     context2.fillRect(0,0, 600, 400);
 
-    //Scoreboard
+   //Scoreboard
     context2.font = "20px Arial";
-    context2.fillStyle = "green";
+    context2.fillStyle = "white";
     context2.fillText("Player: " + playerScore + " Enemy: " + enemyScore, 10, 20);
 
-  //Draw Sprite
-    //context2.drawImage(playerImage, 0, 20, 64,64);
+   //Player Sprite
+    //context2.drawImage(playerImage, 100, 100, 64,64);
     context2.drawImage(playerImage, playerX, playerY, 64,64);
+
+    //Power Up
+    //context2.drawImage(crystalImage, 100, 100, 32, 32);
+    context2.drawImage(crystalImage, crystalX, crystalY, 32, 32);
+
+
+
+//Redraw screen
+    requestAnimationFrame(render);
   }
 
-  //Key Down demo
-  // document.addEventListener('keydown', function(event) {
-  //   console.log(event);
-  //   if (event.key == "w"){
-  //     console.log("W was pressed");
-  //   } else {
-  //     console.log("W was not pressed");
-  //   }
-  // });
+  //Update playerScore when screen clicked
+   canvas2.onclick = function() {
+     playerScore++;
+    }
 
-  //If Syntax
-  // if(thing to test){
-  //   DO THIS IF THING TO TEST IS TRUE
-  // } else {
-  //   DO SOMETHING ELSE
-  // }
+    document.addEventListener('keydown', function (event) {
+      if(event.key === "w"){
+         playerY-=10;
+      }
+      if(event.key === "s"){
+        playerY+=10;
+     }
+     if(event.key === "a"){
+       playerX-=10;
+    }
+    if(event.key === "d"){
+      playerX+=10;
+      }
+    });
 
+    //==============================================================//
 
+    //Math Random demo
+    //console.log(Math.random());
+    //console.log(Math.floor(Math.random()));
+    //console.log(Math.floor(Math.random() * 10));
 
-//Player Move
-document.addEventListener('keydown', function (event) {
-  move(event);
-});
+    //console.log(Math.floor(Math.random() * 450));
+    //console.log(Math.floor(Math.random() * 250));
 
- function move(event){
-   if(event.key === "w"){
-     playerY-=10;
-  }
-  if(event.key === "s"){
-    playerY+=10;
- }
- if(event.key === "a"){
-   playerX-=10;
-}
-if(event.key === "d"){
-  playerX+=10;
- }
-}
+    // var crystalX = Math.floor(Math.random() * 450);
+    // var crystalY = Math.floor(Math.random() * 250);
+
+    //console.log(crystalX);
+    //console.log(crystalY);
